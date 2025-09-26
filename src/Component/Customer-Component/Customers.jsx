3b24cc58bdd9453abf84f81bbd033f8471122d3b
shourav-00 <shouravchowdhury400@gmail.com>
2025-09-27 00:57:03 +0600

    All Done Functionality

--- src/Component/Customer-Component/Customers.jsx
+++ src/Component/Customer-Component/Customers.jsx
@@ -11,7 +11,18 @@ const Customers =({InProgress,setInProgress,rightArea,allCard,setAllCard,setRigh
         <div className='grid grid-cols-2 gap-8 h-full'>
              
         {
-            allCard.map (singleData=><Customer singleData={singleData} InProgress={InProgress} setInProgress={setInProgress}  rightArea={rightArea} setRightArea={setRightArea} allCard={allCard} setAllCard={setAllCard} ></Customer>)
+            allCard.map((singleData, index) => (
+            <Customer 
+                key={singleData.id || index} 
+                singleData={singleData} 
+                InProgress={InProgress} 
+                setInProgress={setInProgress}  
+                rightArea={rightArea} 
+                setRightArea={setRightArea} 
+                allCard={allCard} 
+                setAllCard={setAllCard} 
+            />
+            ))
             
             
         }
